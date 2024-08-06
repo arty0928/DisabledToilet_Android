a8e9796ac0c81a270f1452ae99ce2b94d6e93583
EunseoPark <88071251+arty0928@users.noreply.github.com>
2024-08-06 23:04:41 +0900

    [FEAT] Android 메인 페이지 ( 검색창, 네비게이션 바 틀)

--- app/src/main/java/com/example/disabledtoilet_android/MainActivity.kt
+++ app/src/main/java/com/example/disabledtoilet_android/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.disabledtoilet_android.ui.theme.DisabledToilet_AndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,14 +24,80 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DisabledToilet_AndroidTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { SearchBar() },
+                    bottomBar = { NavigationBar() },
+                    containerColor = Color.White // 배경색 설정
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+//                        Spacer(modifier = Modifier.height(16.dp))
+                        ImageContent(modifier = Modifier.weight(1f))
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ImageContent(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+        contentDescription = "지도 넣기",
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight()
+    )
+}
+
+@Composable
+fun NavigationBar() {
+    BottomAppBar(
+        containerColor = Color.White // 배경색 설정
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(onClick = { /*TODO*/ }) {
+                Text("Button 1")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text("Button 2")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text("Button 3")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ){
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            placeholder = {Text("화장실을 검색하세요")},
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                containerColor = Color.White,
+                focusedBorderColor = Color.Black,
+                unfocusedBorderColor = Color.Black,
+                cursorColor = Color.Black
+            )
+        )
     }
 }
 
--- app/src/main/java/com/example/disabledtoilet_android/ui/theme/Theme.kt
+++ app/src/main/java/com/example/disabledtoilet_android/ui/theme/Theme.kt
@@ -9,18 +9,19 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = MainColor,
+    secondary = SubColor,
+    tertiary = GreenColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = MainColor,
+    secondary = SubColor,
+    tertiary = GreenColor
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
