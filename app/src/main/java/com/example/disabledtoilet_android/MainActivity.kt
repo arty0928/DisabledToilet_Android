3b85cd70713a069bb50f57ba7470d8ba5b93d819
EunseoPark <88071251+arty0928@users.noreply.github.com>
2024-08-08 15:54:21 +0900

    [FEAT] MainPage - NavigationBar Icon

--- app/src/main/java/com/example/disabledtoilet_android/MainActivity.kt
+++ app/src/main/java/com/example/disabledtoilet_android/MainActivity.kt
@@ -11,9 +11,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.disabledtoilet_android.ui.theme.DisabledToilet_AndroidTheme
@@ -36,7 +40,10 @@ class MainActivity : ComponentActivity() {
                             .padding(innerPadding)
                     ) {
 //                        Spacer(modifier = Modifier.height(16.dp))
-                        ImageContent(modifier = Modifier.weight(1f))
+                        //전체 공간에서 weight 비율만큼 지금은 1배
+                        Box(modifier = Modifier.weight(1f)){
+                            ImageContent(modifier = Modifier.fillMaxSize())
+                        }
                     }
                 }
             }
@@ -51,27 +58,53 @@ fun ImageContent(modifier: Modifier = Modifier) {
         contentDescription = "지도 넣기",
         modifier = Modifier
             .fillMaxSize()
-            .fillMaxHeight()
     )
 }
 
 @Composable
 fun NavigationBar() {
+    val navBarHeight = 60.dp
     BottomAppBar(
-        containerColor = Color.White // 배경색 설정
+        containerColor = Color.Cyan,
+        modifier = Modifier.height(navBarHeight)
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(navBarHeight),
+
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Button(onClick = { /*TODO*/ }) {
-                Text("Button 1")
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.align(Alignment.CenterVertically)
+                ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.save_icon),
+                    contentDescription = "찜 아이콘",
+                    modifier = Modifier.size(32.dp)
+                )
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text("Button 2")
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.align(Alignment.CenterVertically)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.plustoilet_icon),
+                    contentDescription = "화장실 추가 아이콘",
+                    modifier = Modifier.size(30.dp)
+                )
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text("Button 3")
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.align(Alignment.CenterVertically)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.mypage_icon),
+                    contentDescription = "마이페이지 아이콘",
+                    modifier = Modifier.size(35.dp)
+                )
             }
         }
     }
@@ -80,14 +113,16 @@ fun NavigationBar() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar() {
+    val textState = remember{ mutableStateOf("") }
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
+            .height(50.dp)
     ){
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = textState.value,
+            onValueChange = {textState.value = it},
             placeholder = {Text("화장실을 검색하세요")},
             modifier = Modifier.fillMaxWidth(),
             shape = RoundedCornerShape(8.dp),
@@ -96,7 +131,8 @@ fun SearchBar() {
                 focusedBorderColor = Color.Black,
                 unfocusedBorderColor = Color.Black,
                 cursorColor = Color.Black
-            )
+            ),
+            textStyle = TextStyle(color = Color.Black)
         )
     }
 }
