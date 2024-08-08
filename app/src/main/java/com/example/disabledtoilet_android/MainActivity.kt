5f53531c9747f035b4f1449f4f07c7bada822285
EunseoPark <88071251+arty0928@users.noreply.github.com>
2024-08-08 17:31:58 +0900

    [FEAT] MainPage - 지도 제외 완성

--- app/src/main/java/com/example/disabledtoilet_android/MainActivity.kt
+++ app/src/main/java/com/example/disabledtoilet_android/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.disabledtoilet_android
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -18,11 +25,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.disabledtoilet_android.ui.theme.DisabledToilet_AndroidTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -32,17 +43,73 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     topBar = { SearchBar() },
                     bottomBar = { NavigationBar() },
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = { /*TODO*/ },
+                            shape = CircleShape,
+                            containerColor = Color.White,
+                            elevation = FloatingActionButtonDefaults.elevation(8.dp),
+                            modifier = Modifier.size(45.dp)
+                        ) {
+                            Icon(Icons.Default.Add, contentDescription = "Add")
+                        }
+                    },
                     containerColor = Color.White // 배경색 설정
-                ) { innerPadding ->
+                ) {
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
-                            .padding(innerPadding)
+                            .padding(top = 55.dp)
                     ) {
-//                        Spacer(modifier = Modifier.height(16.dp))
-                        //전체 공간에서 weight 비율만큼 지금은 1배
-                        Box(modifier = Modifier.weight(1f)){
-                            ImageContent(modifier = Modifier.fillMaxSize())
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(30.dp)
+                                .padding(horizontal = 16.dp)
+                                .background(Color.Red),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+
+                        ){
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = "Search",
+                                modifier = Modifier.size(24.dp)
+                            )
+                            Spacer(modifier = Modifier.weight(1f))
+                            Row(
+                                modifier = Modifier
+                                    .clickable {
+                                        //
+                                    },
+                                verticalAlignment = Alignment.CenterVertically
+                            )
+                            {
+                                Text(
+                                    text = "필터적용",
+                                    style = TextStyle(
+                                        color = Color.Black,
+                                        fontSize = 16.sp,
+                                        fontFamily = FontFamily(Font(R.font.notosanskr_medium))
+                                    )
+                                )
+                                //맨 오른쪽
+                                Image(
+                                    painter = painterResource(id = R.drawable.rightarrow_icon),
+                                    contentDescription = "필터 열기",
+                                    modifier = Modifier.size(24.dp)
+                                )
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(5.dp))
+                        Box(
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxWidth()
+                                .background(Color.LightGray)
+                        ) {
+                            // ImageContent 컴포저블을 여기에 넣을 수 있습니다.
                         }
                     }
                 }
@@ -53,17 +120,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ImageContent(modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(id = R.drawable.ic_launcher_foreground),
-        contentDescription = "지도 넣기",
-        modifier = Modifier
+    Box(
+        modifier = modifier
             .fillMaxSize()
+            .background(Color.DarkGray)
     )
 }
 
 @Composable
 fun NavigationBar() {
-    val navBarHeight = 60.dp
+    val navBarHeight = 80.dp
     BottomAppBar(
         containerColor = Color.Cyan,
         modifier = Modifier.height(navBarHeight)
@@ -72,38 +138,70 @@ fun NavigationBar() {
             modifier = Modifier
                 .fillMaxWidth()
                 .height(navBarHeight),
-
             horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            IconButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.align(Alignment.CenterVertically)
-                ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .clickable { },
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
                 Icon(
                     painter = painterResource(id = R.drawable.save_icon),
                     contentDescription = "찜 아이콘",
                     modifier = Modifier.size(32.dp)
                 )
-            }
-            IconButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.align(Alignment.CenterVertically)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.plustoilet_icon),
-                    contentDescription = "화장실 추가 아이콘",
-                    modifier = Modifier.size(30.dp)
+                Text(
+                    text = "저장",
+                    style = TextStyle(fontSize = 12.sp),
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontFamily = FontFamily(Font(R.font.notosanskr_bold))
+
                 )
             }
-            IconButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.align(Alignment.CenterVertically)
-            ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .clickable { },
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                Icon(
+                    painter = painterResource(id = R.drawable.plustoilet_icon),
+                    contentDescription = "화장실 등록 아이콘",
+                    modifier = Modifier.size(32.dp)
+                )
+                Text(
+                    text = "등록",
+                    style = TextStyle(fontSize = 12.sp),
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontFamily = FontFamily(Font(R.font.notosanskr_bold))
+
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .clickable { },
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
                 Icon(
                     painter = painterResource(id = R.drawable.mypage_icon),
                     contentDescription = "마이페이지 아이콘",
-                    modifier = Modifier.size(35.dp)
+                    modifier = Modifier.size(40.dp)
+                )
+                Text(
+                    text = "MY",
+                    style = TextStyle(fontSize = 12.sp),
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontFamily = FontFamily(Font(R.font.notosanskr_bold))
+
                 )
             }
         }
@@ -113,17 +211,17 @@ fun NavigationBar() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar() {
-    val textState = remember{ mutableStateOf("") }
+    val textState = remember { mutableStateOf("") }
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
             .height(50.dp)
-    ){
+    ) {
         OutlinedTextField(
             value = textState.value,
-            onValueChange = {textState.value = it},
-            placeholder = {Text("화장실을 검색하세요")},
+            onValueChange = { textState.value = it },
+            placeholder = { Text("화장실을 검색하세요") },
             modifier = Modifier.fillMaxWidth(),
             shape = RoundedCornerShape(8.dp),
             colors = TextFieldDefaults.outlinedTextFieldColors(
