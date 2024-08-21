1abe223246a324cf62d34a80ba21273d3230aade
EunseoPark <88071251+arty0928@users.noreply.github.com>
2024-08-22 00:46:23 +0900

    [FEAR] BottomSheet Dialog Show

--- app/src/main/java/com/example/disabledtoilet_android/NearActivity.kt
+++ app/src/main/java/com/example/disabledtoilet_android/NearActivity.kt
@@ -2,9 +2,16 @@ package com.example.disabledtoilet_android
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.graphics.Color
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class NearActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -23,9 +30,42 @@ class NearActivity : AppCompatActivity() {
         val mapButton: ImageButton = findViewById(R.id.map_icon)
 
         // Set an OnClickListener to call onBackPressed() when the button is clicked
-        backButton.setOnClickListener {
-            val intent = Intent(this, NearActivity::class.java)
-            startActivity(intent)
+        mapButton.setOnClickListener {
+            showBottomSheetDialog()
         }
     }
+
+    private fun showBottomSheetDialog() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+
+        val bottomSheetView = layoutInflater.inflate(R.layout.detail_bottomsheet, null)
+
+        // Set the background of the dialog to be transparent
+        bottomSheetDialog.setOnShowListener { dialog ->
+            val bottomSheetDialogInstance = dialog as BottomSheetDialog
+            val bottomSheet = bottomSheetDialogInstance.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+
+            bottomSheet?.let {
+                // Set the background of the bottom sheet to transparent
+                it.setBackgroundResource(android.R.color.transparent)
+
+                // Set the height of the bottom sheet to 250dp
+                val behavior = BottomSheetBehavior.from(it)
+                val displayMetrics = resources.displayMetrics
+                val heightInPx = (250 * displayMetrics.density).toInt() // Convert 250dp to pixels
+                behavior.peekHeight = heightInPx
+                it.layoutParams.height = heightInPx
+                behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+            
+            //배경 어두워지는 거 제거
+            bottomSheetDialogInstance.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        }
+
+        bottomSheetDialog.setContentView(bottomSheetView)
+        bottomSheetDialog.window?.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
+        bottomSheetDialog.show()
+    }
+
+
 }
